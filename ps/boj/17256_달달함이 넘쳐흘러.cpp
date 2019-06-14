1a12d3bc5eae32f898c83a4c0861fc3fa2bab3e7
hansol <onesorry49@gmail.com>
2019-06-14 22:45:43 +0900

    PS: daily ps with cpp

---
+++ ps/boj/17256_달달함이 넘쳐흘러.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// 17256번 달달함이 넘쳐흘러
+// 분류: 수학
+// 문제
+//		방정식 풀기
+// 풀이:
+//		이항해서 구한다.
+int main() {
+	int a, b, c;
+	int d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << d - c << " " << e / b << " " << f - a << '\n';
+}
