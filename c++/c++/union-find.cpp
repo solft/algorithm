16f5ca6f0164e767ba2ed83ce137030fb966713b
hansol <onesorry49@gmail.com>
2019-06-27 23:10:18 +0900

    modified misspell

--- c++/c++/union-find.cpp
+++ c++/c++/union-find.cpp
@@ -1,22 +1,22 @@
 #define NODE_SIZE 100
-int parant[NODE_SIZE];
+int parent[NODE_SIZE];
 int ranking[NODE_SIZE] = { 0, };
 
 int Find(int x) {
-	if (parant[x] == x) return x;
-	return parant[x] = Find(parant[x]);
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
 }
 
 void Union(int x, int y) {
 	x = Find(x);
 	y = Find(y);
 	if (x == y) return;
-	if (ranking[x] < ranking[y]) parant[x] = y;
-	else parant[y] = x;
+	if (ranking[x] < ranking[y]) parent[x] = y;
+	else parent[y] = x;
 	if (ranking[x] == ranking[y]) ranking[x]++;
 }
 
 int main() {
 	for (int i = 0; i < NODE_SIZE; i++)
-		parant[i] = i;
+		parent[i] = i;
 }
