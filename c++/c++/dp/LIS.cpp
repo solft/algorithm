7e4fe4980626b97fd8c802cc442edad4f13ac4e8
hansol <onesorry49@gmail.com>
2019-06-28 20:38:25 +0900

    LIS: modify 2 solutions find lis list and find fast length

--- c++/c++/dp/LIS.cpp
+++ c++/c++/dp/LIS.cpp
@@ -14,6 +14,30 @@ int main() {
 		cout << s << ' ';
 	cout << '\n';
 
+	// O(N^2)로 구하기
+	vector<int> dp;
+	vector<vector<int>> lisList;
+	int n = sequence.size();
+	dp.resize(n);
+	lisList.resize(n);
+	for (int i = 0; i < n; i++) {
+		if (dp[i] == 0) dp[i] = 1;
+		for (int j = 0; j < i; j++)
+			if (sequence[i] > sequence[j])
+				if (dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
+					lisList[i] = lisList[j];
+					lisList[i].push_back(sequence[i]);
+				}
+	}
+
+	cout << "dp를 활용한 LIS 길이 = " << dp[n - 1] << '\n';
+	for (auto k : lisList[n - 1])
+		cout << k << ' ';
+	cout << '\n';
+
+
+	// 길이만 빠르게 찾기
 	for (auto num : sequence) {
 		if (lis.empty() || lis.back() < num)
 			lis.push_back(num);
@@ -23,7 +47,5 @@ int main() {
 		}
 	}
 
-	for (auto s : lis)
-		cout << s << ' ';
-	cout << '\n';
+	cout << "lower_bound를 활용한 LIS 길이 = " << lis.size() << '\n';
 }
